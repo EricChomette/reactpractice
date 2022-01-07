f700784f71e47bb9e9c370ad6b3a5ad570de8867
EricChomette <eric.chomette@gmail.com>
2022-01-07 14:44:39 +0100

    card react flat

--- src/components/hello.js
+++ src/components/hello.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Hello extends React.Component {
   render() {
-    return <div>Hello {this.props.firstname} {this.props.firstname}!</div>;
+    return <div>Hello {this.props.firstname} {this.props.lastname}!</div>;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Hello from './components/hello';
-
-
+// import Hello from './components/hello';
 // import reportWebVitals from './reportWebVitals';
 
-const root = <div>
-  <Hello firstname="Boris" lastname="Papillard" />
-  <Hello firstname="Romain" lastname="Papillard" />
-</div>
+// const root = <div>
+//   <Hello firstname="Boris" lastname="Papillard" />
+//   <Hello firstname="Romain" lastname="Papillard" />
+// </div>
 
 
 
-ReactDOM.render(root ,document.getElementById('root')
+ReactDOM.render(<App />,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
