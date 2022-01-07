62debf08c9ddc957ac658ad2339ef52e1b314ec4
EricChomette <eric.chomette@gmail.com>
2022-01-07 09:35:46 +0100

    savenodeversioninit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './components/hello';
+
+
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<Hello />,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
